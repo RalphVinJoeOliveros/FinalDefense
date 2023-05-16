f2dc512396753d8cdeb1fa76ffc719010f5f478a
Ralph Vin Joe Oliveros <oliveros.ralphvinjoe@ici.edu.ph>
2023-05-16 20:12:00 +0800

    Update student-signup_page.php

--- student-signup_page.php
+++ student-signup_page.php
@@ -159,7 +159,7 @@ if(isset($_SESSION['lrn'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
+    <title>Registration Form</title>
     <style>
 *{
     font-family: sans-serif;
